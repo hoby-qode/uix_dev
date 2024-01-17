3c584fdf3f3ba29707bb755e55e15bde31cc86a1
marcel <marcel@medialibs.com>
2024-01-17 16:11:44 +0300

    Supprestion des codes inactifs sur le fichier next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true
@@ -8,36 +7,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'uixdev.s193304.mos2.atester.fr',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-      {
-        protocol: 'http',
-        hostname: "*.gravatar.com"
-      }
-    ],
-  },
-  async headers() {
-    return [
-        {
-            // matching all API routes
-            source: "/wp-json/:path*",
-            headers: [
-                { key: "Access-Control-Allow-Credentials", value: "true" },
-                { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-        }
-    ]
-  }
+  output: 'export',
 }
 
 module.exports = nextConfig
